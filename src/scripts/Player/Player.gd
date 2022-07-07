d774f353336cd5b5f1a4eae629905cc4cc735e6a
nathan center <nathanjcenter@gmail.com>
2022-07-07 16:35:54 -0500

    fixed commits and collision shape

--- src/scripts/Player/Player.gd
+++ src/scripts/Player/Player.gd
@@ -1,17 +1,12 @@
 extends KinematicBody
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+
 var velocity=Vector3(0,0,0)
 export var  speed=1
 var gravity=0
 var jump=5
 var bolt=0
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
 func _physics_process(delta):
 	if Input.is_action_pressed("ui_right") and  Input.is_action_pressed("ui_left"):
@@ -32,6 +27,3 @@ func _physics_process(delta):
 	
 	
 	move_and_slide(velocity,Vector3.UP)
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
--- src/scripts/bolt/bolt.gd
+++ src/scripts/bolt/bolt.gd
@@ -1,25 +1,18 @@
 extends Area
 
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
+
 var getMagnet=false
 #var position=Vector3()
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
 
 #adding random number
 var random=RandomNumberGenerator.new()
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
+
 func _physics_process(delta):
 	if getMagnet == false:
 		#makes the bolts fall due to the y axis
 		translation.y
-		#print(translation)
+		
 	var bodies=get_overlapping_areas()
 	for body in bodies:
 		if body.name=="AreaPlayer":
