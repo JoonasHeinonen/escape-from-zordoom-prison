ee0c72c64ddfb31ffae275f4c0e1b4a33928fb36
Joonas Heinonen <joonas.heinonen@edu.turkuamk.fi>
2022-10-23 23:16:01 +0300

    feature/weapon-wheel-functionality: Locked the latest button to be focused on when opening inventory.

--- src/scripts/Player/Player.gd
+++ src/scripts/Player/Player.gd
@@ -1,30 +1,41 @@
 extends KinematicBody
 
-onready var projectile = preload("res://scenes/Projectiles/BlasterProjectile.tscn")
+onready var projectile 	  = preload("res://scenes/Projectiles/BlasterProjectile.tscn")
 onready var hand_instance = $Sprite3D/HandInstance
-onready var camera = $Camera
+onready var camera  	  = $Camera
 
-export var speed = 1
+export var speed 		  = 1
 
 var state_machine
+var active_weapon_button
 
-var velocity = Vector3(0,0,0)
+var velocity 			  = Vector3(0,0,0)
 
-var gravity = 4
-var jump = 4
-var bolt = 0
+var gravity 			  = 4
+var jump 				  = 4
+var bolt 				  = 0
+var current_weapon_index  = 0
+var weapons				  = 0
 
-var alive = true
-var on_inventory = false
+var alive 				  = true
+var on_inventory 		  = false
 
-var ray_origin = Vector3()
-var ray_end = Vector3()
+var ray_origin  		  = Vector3()
+var ray_end 			  = Vector3()
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	$ShootTimer.connect("timeout", self, "_on_ShootTimer_timeout")
 	$ShootTimer.start()
 	state_machine = $AnimationTree.get("parameters/playback")
+	"""
+	for i in $PlayerUI/InventoryContainer.get_children():
+			for button in i.get_children():
+				
+	"""
+	$PlayerUI/InventoryContainer.visible = false
+	active_weapon_button =  $PlayerUI/InventoryContainer/Button_0/button_0
 	walk(0, 1, (-1) * 0.1, -2)
 
 func _physics_process(delta):
@@ -43,10 +54,11 @@ func _physics_process(delta):
 			state_machine.travel("Angela_Still")
 		if is_on_floor() and Input.is_action_just_pressed("jump"):
 			velocity.y = jump
-		if Input.is_action_just_released("ui_home"):
+		if Input.is_action_just_released("ui_inventory"):
 			on_inventory = true
+			active_weapon_button.grab_focus()
 	elif on_inventory == true:
-		if Input.is_action_just_released("ui_home"):
+		if Input.is_action_just_released("ui_inventory"):
 			on_inventory = false
 	
 	if not is_on_floor():
@@ -65,7 +77,7 @@ func _process(delta):
 
 	# Get current physics state.
 	var space_state = get_world().direct_space_state
-
+	
 	var ray_length = 100000
 	var mouse_pos = get_viewport().get_mouse_position()
 	var from = camera.project_ray_origin(mouse_pos)
@@ -77,7 +89,7 @@ func _process(delta):
 	if y_position < -3:
 		# alive = false
 		pass
-		
+
 	# Inventory functionalities.
 	if on_inventory:
 		velocity.x = 0
@@ -110,8 +122,52 @@ func shoot():
 	get_parent().add_child(bullet)
 	bullet.global_transform = $Sprite3D/MeshInstance/HandInstance/Hand/WeaponPlaceHolder/WeaponMuzzle.global_transform
 
+# Change the current active weapon
+func change_weapon(weapon):
+	pass
+
 # Used to debug the rotation values.
 func debug_rotation_values(x, y, z):
 	var values = "Rotation values: %s %s %s."	
 	var args = values % [x, y, z]
 	print(args)
+
+# Button_0 pressed.
+func _on_button_0_pressed():
+	active_weapon_button =  $PlayerUI/InventoryContainer/Button_0/button_0
+	on_inventory = false
+
+# Button_1 pressed.
+func _on_button_1_pressed():
+	active_weapon_button =  $PlayerUI/InventoryContainer/Button_1/button_1
+	on_inventory = false
+
+# Button_2 pressed.
+func _on_button_2_pressed():
+	active_weapon_button =  $PlayerUI/InventoryContainer/Button_2/button_2
+	on_inventory = false
+
+# Button_3 pressed.
+func _on_button_3_pressed():
+	active_weapon_button =  $PlayerUI/InventoryContainer/Button_3/button_3
+	on_inventory = false
+
+# Button_4 pressed.
+func _on_button_4_pressed():
+	active_weapon_button =  $PlayerUI/InventoryContainer/Button_4/button_4
+	on_inventory = false
+
+# Button_5 pressed.
+func _on_button_5_pressed():
+	active_weapon_button =  $PlayerUI/InventoryContainer/Button_5/button_5
+	on_inventory = false
+
+# Button_6 pressed.
+func _on_button_6_pressed():
+	active_weapon_button =  $PlayerUI/InventoryContainer/Button_6/button_6
+	on_inventory = false
+
+# Button_7 pressed.
+func _on_button_7_pressed():
+	active_weapon_button =  $PlayerUI/InventoryContainer/Button_7/button_7
+	on_inventory = false
