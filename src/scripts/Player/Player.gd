24e522ebd2ce904a2af7a1047b5010f4e4781b3f
Joonas Heinonen <joonas.heinonen@edu.turkuamk.fi>
2022-12-18 23:41:20 +0200

    feature/vendor-system: Button removed, vendor images added.

--- src/scripts/Player/Player.gd
+++ src/scripts/Player/Player.gd
@@ -84,6 +84,7 @@ func _physics_process(delta):
 			velocity.y = jump
 
 	if Input.is_action_just_released("ui_accept"):
+		print(Globle.WPNS[0])
 		Globle.update_vendor()
 
 	if not is_on_floor():
@@ -126,10 +127,19 @@ func set_vendor_weapons(weapons_for_sale):
 		n.queue_free()
 
 	for wpn_for_sale in weapons_for_sale:
+		var wpn_name = ""
+		var unwanted_chars = ["_"]
 		var btn = gun_btn.instance()
 		btn.set_wpn_for_sale(wpn_for_sale)
-		btn.set_label(wpn_for_sale)
+		
+		# Takes the chars from the wpn_for_sale.
+		for c in unwanted_chars:
+			wpn_name = wpn_for_sale.replace(c, " ")
+		wpn_name = wpn_name.to_upper()
+
+		btn.set_label(wpn_name)
 		btn.connect("pressed", self, "_on_Vendor_Choice_pressed", [btn, btn.wpn_for_sale])
+		btn.connect("focus_entered", self, "_on_VendorWeaponButton_focus_entered", [btn, btn.wpn_for_sale])
 		$PlayerUI/VendorContainer/WeaponsForSale/CenterRow/Buttons.add_child(btn)
 
 # Sets all the in the inventory.
@@ -197,6 +207,28 @@ func walk(vel, scale, weapon_translation, hand_translation):
 	$Sprite3D/MeshInstance.scale.x = weapon_translation
 	$Sprite3D/MeshInstance/HandInstance.scale.y = hand_translation
 	$Sprite3D/MeshInstance.rotate_x(270)
+
+# Purchases weapon when a weapon button is pressed.
+func purchase_weapon(wpn_price : int, wpn, btn):
+	print("Purchasing...")
+	if (Globle.bolts >= wpn_price):
+		get_tree().paused = false
+		Globle.current_weapons.append(wpn)
+		Globle.update_vendor()
+		Globle.bolts -= wpn_price
+		btn.queue_free()
+		set_vendor_weapons(Globle.weapons_for_sale)
+		get_tree().paused = true
+	else:
+		print("Insufficient funds...")
+
+# Updates the vendor data once a weapon is highlighted.
+func update_vendor_data(wpn_name, wpn_price : int, wpn_desc):
+	var weapon_sprite_path = "res://resources/images/weapons/vendor/" + wpn_name + "_vendor.png"
+	print("Weapon name: " + str(wpn_name))
+	$PlayerUI/VendorContainer/WeaponDescriptionPanel/HBoxContainer/WeaponPrice.text = str(wpn_price)
+	$PlayerUI/VendorContainer/WeaponDescriptionPanel/WeaponDescription.text = str(wpn_desc)
+	$PlayerUI/VendorContainer/WeaponDescriptionPanel/WpnImageContainer/WpnImageBackground/WpnImage.texture = load(weapon_sprite_path)
 
 # Shooting functionality for the edge blaster.
 func shoot_edge_blaster():
@@ -267,8 +299,67 @@ func _on_ShootTimer_timeout():
 
 # Loads the scene defined to a particular button.
 func _on_Vendor_Choice_pressed(button, wpn):
-	Globle.current_weapons.append(wpn)
-	Globle.update_vendor()
+	match (wpn):
+		"edge_blaster":
+			purchase_weapon(Globle.WPNS[1][0], wpn, button)
+		"blitz_gun":
+			purchase_weapon(Globle.WPNS[1][1], wpn, button)
+		"gravity_bomb":
+			purchase_weapon(Globle.WPNS[1][2], wpn, button)
+		"negotiator":
+			purchase_weapon(Globle.WPNS[1][3], wpn, button)
+		"pulse_rifle":
+			purchase_weapon(Globle.WPNS[1][4], wpn, button)
+		"ry3no":
+			purchase_weapon(Globle.WPNS[1][5], wpn, button)
+		"sheepinator":
+			purchase_weapon(Globle.WPNS[1][6], wpn, button)
+
+# Acts when a vendor weapon button is highlighted.
+func _on_VendorWeaponButton_focus_entered(button: Button, wpn):
+	match (wpn):
+		"edge_blaster":
+			update_vendor_data(
+				Globle.WPNS[0][0],
+				Globle.WPNS[1][0],
+				Globle.WPNS[2][0]
+			)
+		"blitz_gun":
+			update_vendor_data(
+				Globle.WPNS[0][1],
+				Globle.WPNS[1][1],
+				Globle.WPNS[2][1]
+			)
+		"gravity_bomb":
+			update_vendor_data(
+				Globle.WPNS[0][2],
+				Globle.WPNS[1][2],
+				Globle.WPNS[2][2]
+			)
+		"negotiator":
+			update_vendor_data(
+				Globle.WPNS[0][3],
+				Globle.WPNS[1][3],
+				Globle.WPNS[2][3]
+			)
+		"pulse_rifle":
+			update_vendor_data(
+				Globle.WPNS[0][4],
+				Globle.WPNS[1][4],
+				Globle.WPNS[2][4]
+			)
+		"ry3no":
+			update_vendor_data(
+				Globle.WPNS[0][5],
+				Globle.WPNS[1][5],
+				Globle.WPNS[2][5]
+			)
+		"sheepinator":
+			update_vendor_data(
+				Globle.WPNS[0][6],
+				Globle.WPNS[1][6],
+				Globle.WPNS[2][6]
+			)
 
 # Change weapon to Edge Blaster.
 func _on_WeaponSlot1_pressed():
