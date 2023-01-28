f2f9b9aa79e7040ef3cd28b74a22a8c60c521415
Nathan Center <nathanjcenter@gmail.com>
2023-01-28 13:28:57 -0700

    added flame.png

--- src/scripts/effects/bullets/GravityBombProjectile.gd
+++ src/scripts/effects/bullets/GravityBombProjectile.gd
@@ -5,4 +5,12 @@ var velocity = Vector3(1,-1,0)
 #https://www.youtube.com/watch?v=p6OQ7XVsiKw
 
 func _ready():
+	$KillTimer.connect("timeout", self, "_on_KillTimer_timeout")
+	$KillTimer.start()
 	apply_impulse(Vector3.ZERO,velocity*10)
+
+#https://www.youtube.com/watch?v=F1Fyj3Lh_Pc&t=239s
+#Explosition effects
+func _on_GravityBombArea_body_entered(body):
+	queue_free()
+	pass # Replace with function body.
