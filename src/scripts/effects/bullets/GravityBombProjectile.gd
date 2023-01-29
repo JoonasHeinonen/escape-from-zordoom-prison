763c25fe17d1b3c81624a7e6e186930e0451faf3
Nathan Center <nathanjcenter@gmail.com>
2023-01-29 10:40:14 -0700

    fixed gravity bomb bugs

--- src/scripts/effects/bullets/GravityBombProjectile.gd
+++ src/scripts/effects/bullets/GravityBombProjectile.gd
@@ -3,14 +3,16 @@ extends RigidBody
 
 var velocity = Vector3(1,-1,0)
 #https://www.youtube.com/watch?v=p6OQ7XVsiKw
-
+onready var gravity_bomb_explosion = preload("res://scenes/Effects/ProjectileEffects/GravityBombExplosion.tscn")
 func _ready():
 	$KillTimer.connect("timeout", self, "_on_KillTimer_timeout")
 	$KillTimer.start()
-	apply_impulse(Vector3.ZERO,velocity*10)
+	apply_impulse(Vector3.ZERO,velocity*8)
 
 #https://www.youtube.com/watch?v=F1Fyj3Lh_Pc&t=239s
 #Explosition effects
 func _on_GravityBombArea_body_entered(body):
+	var explosion = gravity_bomb_explosion.instance()
+	get_tree().current_scene.add_child(explosion)
+	explosion.global_transform = $Explosion.global_transform
 	queue_free()
-	pass # Replace with function body.
---
+++ src/scripts/effects/explosions/gravity_bomb_explosion.gd
@@ -0,0 +1,5 @@
+extends KinematicBody
+
+func _ready():
+	print("test")
+	pass
