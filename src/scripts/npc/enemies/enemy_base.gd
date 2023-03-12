9a1b5eab6d994a285233c9524a065763c295ea70
Joonas Heinonen <joonas.heinonen@edu.turkuamk.fi>
2023-03-12 23:18:34 +0200

    Initial enemy
    Initial enemy placeholder commit.

---
+++ src/scripts/npc/enemies/enemy_base.gd
@@ -0,0 +1,13 @@
+extends KinematicBody
+
+var enemy_health : int
+var enemy_speed  : int
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
