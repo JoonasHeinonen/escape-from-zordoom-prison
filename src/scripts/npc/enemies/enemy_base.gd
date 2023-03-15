588a9c947b162ed5d282d1bf73ca2055a629d861
Joonas Heinonen <joonas.heinonen@edu.turkuamk.fi>
2023-03-15 21:47:48 +0200

    Active aiming radical
    Applied active aiming radical for the enemy.

--- src/scripts/npc/enemies/enemy_base.gd
+++ src/scripts/npc/enemies/enemy_base.gd
@@ -1,19 +1,24 @@
 extends KinematicBody
 
+onready var radical 		  = preload("res://scenes/UI/GreenTargetRadical.tscn")
+
 export var enemy_health : int = 10
 export var enemy_speed  : int = 10
 
+var meta_name : String 		  = ""
 var velocity 				  = Vector3(0, 0, 0)
 
 var state_machine
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	meta_name = "enemy"
 	state_machine = $EnemyAnimationTree.get("parameters/playback")
+	self.set_meta("type", "destroyable")
+	self.set_meta("name", "bolt crate")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	print(state_machine.get_current_node())
+func _process(_delta):
 	velocity.y = -4
 	move_and_slide(velocity, Vector3.UP)
 	
@@ -40,3 +45,24 @@ func _on_AreaEnemy_area_entered(area):
 	if (area.name == "ProjectileExplosionArea"):
 		state_machine.travel("Enemy_Damage")
 		damage_enemy(2)
+
+func add_active_radical():
+	var g_t_r = radical.instance()
+	print(self)
+	if (!self.has_node("res://scenes/UI/GreenTargetRadical.tscn")):
+		self.add_child(g_t_r)
+		print("Added a child")
+
+func remove_active_radical():
+	var d_l = self.get_children()
+	for c in d_l:
+		if (c.name == "GreenTargetRadical"):
+			c.queue_free()
+
+func _on_Enemy_mouse_entered():
+	print("Enemy here!")
+	add_active_radical()
+
+func _on_Enemy_mouse_exited():
+	print("No more enemy")
+	remove_active_radical()
