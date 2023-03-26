20f72b20b001e8794680bf82f1a4e5a455ce06d8
Nathan Center <nathanjcenter@gmail.com>
2023-03-26 10:14:36 -0700

    issues with ammo can

--- src/scripts/bolt/bolt.gd
+++ src/scripts/bolt/bolt.gd
@@ -3,11 +3,12 @@ extends Area
 export (String, "bolt", "ammo") var type
 
 # var position 			   = Vector3()
-var active = false
+var timer 				   = Timer.new()
+
+var active 				   = false
 var getMagnet 			   = false
 var random 	  			   = RandomNumberGenerator.new() # Adding random number.
 var collectible_image_path = "res://resources/images/collectibles/"
-var timer = Timer.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -15,8 +16,8 @@ func _ready():
 	var bolt_index = str(random.randi_range(0,2))
 	var bolt_file_name = "bolt_" + bolt_index + ".png"
 	var resource = null
-	connect("body_entered" , self , "_on_Ammo_body_entered")
 	connect("body_exited" , self , "_on_Ammo_body_exited")
+	connect("body_entered" , self , "_on_Ammo_body_entered")
 	print(self.name)
 	
 	if (type == "bolt"):
@@ -65,29 +66,29 @@ func _physics_process(delta):
 						bod.collect_bolt(random.randi_range(0,1), "ammo")
 				queue_free()
 				
-func _process(delta):	
+func _process(delta):
+	
 	if (type == "bolt"):
 		#hides the notification
 		$CanvasLayer/Ui_notification.hide()
 	elif (type == "ammo"):	
 		#shows the notification
+		timer.wait_time+=delta
 		$CanvasLayer/Ui_notification.visible=active
 		
-func _on_Ammo_body_entered(body):
-	if body.name == "player":
-		active=true
-		print(active)
-		print("body has entered the ammo can")
-		
 func _on_Ammo_body_exited(body):
-	timer.connect("timeout",self,"do_this")
-	timer.wait_time = 5
-	timer.one_shot = true
-	add_child(timer)
-	timer.start()
-
-	print (timer)
-	if body.name == "player" :
+	if (timer.wait_time==3):
 		active=false
+		print(timer.wait_time)
 		print("body has exited the ammo can")
 
+func _on_Ammo_body_entered(body):
+	
+	print("body has entered the ammo can")
+	timer = Timer.new()
+	add_child(timer)
+	timer.autostart = true
+	timer.wait_time = 1
+	timer.connect("timeout", self, "_timeout")
+	active=true
+	print("Timed out!")
