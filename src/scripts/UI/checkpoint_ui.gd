302608f372c24498a2ea3c46cb17603fe041cfa1
Nathan Center <nathanjcenter@gmail.com>
2023-04-02 10:55:24 -0700

    box goes away after the player exits the area 3d in the checkpoint

--- src/scripts/UI/checkpoint_ui.gd
+++ src/scripts/UI/checkpoint_ui.gd
@@ -9,18 +9,17 @@ func _ready():
 	connect("body_entered",self,"_on_checkpoint_body_entered")
 	connect("body_exited",self,"_on_checkpoint_body_exited")
 	$checkpoint/Ui_notification.visible=false
-func _process(delta):	
-	pass
-	
 	
 func _on_checkpoint_body_entered(body):
+	
 	if body.name == "player":
 		$checkpoint/Ui_notification.show()
 		print("player has entered the checkpoint box")
-		
+		active=true
 func _on_checkpoint_body_exited(body):
+	#after the player exits the area the box goes away
 	if body.name == "player":
 		active=false
 		print("player has exited the checkpoint box")
 		$checkpoint/Ui_notification.hide()
-		$checkpoint.hide()
+		queue_free()
