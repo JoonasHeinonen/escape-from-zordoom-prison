53bea982e96c36e3f1529010aadc59c425aaa9f4
Nathan Center <nathanjcenter@gmail.com>
2023-04-08 11:49:20 -0700

    cleaned up some files and added an if statement

--- src/scripts/Player/New SpawnPoint.gd
+++ src/scripts/Player/New SpawnPoint.gd
@@ -2,5 +2,5 @@ extends Area
 
 # This updates the globle varable to change the postion of the player
 func _on_New_SpawnPoint_body_entered(body):
-	
-	Globle.update_spawn(global_transform.origin)
+	if body.name == "player":
+		Globle.update_spawn(global_transform.origin)
