3d8619482118983e7fbca2b81d32af703ff26ca5
Joonas Heinonen <joonas.heinonen@edu.turkuamk.fi>
2023-04-16 22:54:19 +0300

    hotfix/talking-heads: Fixed the talking heads.

--- src/scripts/npc/npc.gd
+++ src/scripts/npc/npc.gd
@@ -9,34 +9,68 @@ func _ready():
 	connect("body_entered", self, "_on_NPC_body_entered")
 	connect("body_exited", self, "_on_NPC_body_exited")
 
+# Called when there is an input event.
 func _input(event):
-	if get_node_or_null('DialogNode') == null:
-			if Input.is_action_just_pressed("ui_accept") and active == true: 
-				match(value):
-					(0):
-						get_tree().paused = true
-						var dialog = Dialogic.start('timeline-1')
-						dialog.pause_mode = Node.PAUSE_MODE_PROCESS
-						dialog.connect('timeline_end', self, 'unpause')
-						add_child(dialog)
-					(2):
+	if (Globle.player_character == "Angela"):
+		if get_node_or_null('DialogNode') == null:
+				if Input.is_action_just_pressed("ui_accept") and active==true: 
+					match(value):
+						(0):
+							get_tree().paused=true
+							var dialog=Dialogic.start('timeline-1')
+							dialog.pause_mode=Node.PAUSE_MODE_PROCESS
+							dialog.connect('timeline_end',self,'unpause')
+							add_child(dialog)
+							print(active)
+						(2):
 						
-						get_tree().paused = true
-						var dialog = Dialogic.start('timeline-2')
-						dialog.pause_mode = Node.PAUSE_MODE_PROCESS
-						dialog.connect('timeline_end', self, 'unpause')
-						add_child(dialog)
-					(4):
+							get_tree().paused=true
+							var dialog=Dialogic.start('timeline-2')
+							dialog.pause_mode=Node.PAUSE_MODE_PROCESS
+							dialog.connect('timeline_end',self,'unpause')
+							add_child(dialog)
+							print(active)
+						(4):
 						
-						get_tree().paused = true
-						var dialog = Dialogic.start('timeline-3')
-						dialog.pause_mode = Node.PAUSE_MODE_PROCESS
-						dialog.connect('timeline_end', self, 'unpause')
-						add_child(dialog)
-				value += 1
-				if value > 4 and active == true:
-					value = 4
-					active = false
+							get_tree().paused=true
+							var dialog=Dialogic.start('timeline-3')
+							dialog.pause_mode=Node.PAUSE_MODE_PROCESS
+							dialog.connect('timeline_end',self,'unpause')
+							add_child(dialog)
+							print(active)
+					value+=1
+					if value>4 and active == true:
+						value=4
+						active=false
+	if (Globle.player_character == "Rivet"): 
+		if get_node_or_null('DialogNode') == null:
+				if Input.is_action_just_pressed("ui_accept") and active == true: 
+					match(value):
+						(0):
+							get_tree().paused=true
+							var dialog=Dialogic.start('timeLine-Rivet-1')
+							dialog.pause_mode=Node.PAUSE_MODE_PROCESS
+							dialog.connect('timeline_end',self,'unpause')
+							add_child(dialog)
+							print(active)
+						(2):
+							get_tree().paused=true
+							var dialog=Dialogic.start('timeLine-Rivet-2')
+							dialog.pause_mode=Node.PAUSE_MODE_PROCESS
+							dialog.connect('timeline_end',self,'unpause')
+							add_child(dialog)
+							print(active)
+						(4):
+							get_tree().paused=true
+							var dialog=Dialogic.start('timeLine-Rivet-3')
+							dialog.pause_mode=Node.PAUSE_MODE_PROCESS
+							dialog.connect('timeline_end',self,'unpause')
+							add_child(dialog)
+							print(active)
+					value+=1
+					if value>4 and active==true:
+						value=4
+						active=false
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
