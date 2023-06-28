d322b3a0ab00a961081726f5056df34a8dc25546
Nathan Center <34755641+NathanCenter@users.noreply.github.com>
2023-06-27 20:13:43 -0700

    raycast sees or not sees player

--- src/scripts/npc/enemies/RayCast_nef_trooper_head.gd
+++ src/scripts/npc/enemies/RayCast_nef_trooper_head.gd
@@ -1,6 +1,8 @@
 extends RayCast
 onready var  laser_attack =  preload("res://scenes/Projectiles/nef_head_laser.tscn")
 var target: Player = null
+
+var attack 
 #to do: make the shooting animation and have it activate when the nef_head sees the player
 #to do : make so that when it shoots but it being odd
 #https://www.youtube.com/watch?v=04A7pUkhx3E
@@ -8,6 +10,7 @@ func _physics_process(_delta:float) -> void:
 	if is_colliding():
 		if get_collider() is Player:
 			print("sees player")
-			var attack = laser_attack.instance()
-			get_parent().add_child(attack)
-			
+		elif get_collider() != Player:
+			print("is not player")
+	else :
+		print("doesnt see player")
