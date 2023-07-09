33c1af5a105c630f0bc1ef398bb9e6342a1a5bd1
Joonas Heinonen <joonas.heinonen@edu.turkuamk.fi>
2023-07-09 15:16:03 +0300

    feature/expand-test-level: Distance detection implemented.

--- src/scripts/npc/enemies/enemy_base.gd
+++ src/scripts/npc/enemies/enemy_base.gd
@@ -8,6 +8,7 @@ export var enemy_health : int  = 10
 export var enemy_speed  : int  = 10
 
 var alerted   : bool 		  = false
+var in_range  : bool 		  = false
 var meta_name : String 		  = ""
 var velocity  : Vector3 	  = Vector3(0, 0, 0)
 
@@ -25,9 +26,6 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
-	velocity.y = -4
-	move_and_slide(velocity, Vector3.UP)
-	
 	# Set the animation back to "Enemy_Idle".
 	if (state_machine.get_current_node() == "Enemy_Damage"):
 		if (state_machine.get_current_play_position() >= 0.2):
@@ -39,9 +37,17 @@ func _process(_delta):
 	if direction == "Right" : $EnemySprite.flip_h = false
 	elif direction == "Left": $EnemySprite.flip_h = true
 
-	# When the enemy character is aware of the player's vicinity.
-	if alerted:
-		turn_enemy(player.translation.x, self.translation.x)
+func _physics_process(delta):
+	if not is_on_floor():
+		velocity.y = -4
+		state_machine.travel("Enemy_Fall")
+	else:
+		# When the enemy character is aware of the player's vicinity.
+		if alerted:
+			turn_enemy(player.translation.x, self.translation.x)
+		else:
+			state_machine.travel("Enemy_Idle")
+	move_and_slide(velocity, Vector3.UP)
 
 # Called when damage is dealt to the enemy.
 func damage_enemy(health : int):
@@ -77,8 +83,12 @@ func remove_active_radical():
 
 # Walking functionality.
 func walk(vel):
-	state_machine.travel("Enemy_Walk")
-	velocity.x = vel
+	if !in_range:
+		state_machine.travel("Enemy_Walk")
+		velocity.x = vel
+	else:
+		state_machine.travel("Enemy_Idle")
+		velocity.x = 0
 
 # When an area collides with this area.
 # Used for a projectile collision now.
@@ -96,3 +106,16 @@ func _on_Enemy_mouse_entered():
 # Act when the mouse has left the base.
 func _on_Enemy_mouse_exited():
 	remove_active_radical()
+
+# Act when the distance is adequate to the player.
+func _on_DetectDistance_body_entered(body):
+	if (body == player): print("Detection when entering: ", in_range)
+	if (body == player && alerted):
+		in_range = true
+
+# Act when the distance is too far to the player.
+func _on_DetectDistance_body_exited(body):
+	if (body == player): print("Detection when exiting:  ", in_range)
+	if (body == player && alerted):
+		in_range = false
+		turn_enemy(player.translation.x, self.translation.x)
