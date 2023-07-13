74105b58098db09b6a13f78ea7c5a7e92305a541
Nathan Center <34755641+NathanCenter@users.noreply.github.com>
2023-07-12 19:44:43 -0700

    nef_head dies
    when the player shoots at the nef_head he dies and when the nef_head respawns he comes back

--- src/scripts/npc/enemies/nef_head_enemy_ai.gd
+++ src/scripts/npc/enemies/nef_head_enemy_ai.gd
@@ -19,6 +19,8 @@ onready var ray =$Sprite3D/player_finding
 
 var laser_attack_scene = preload("res://scenes/Projectiles/nef_head_laser.tscn")
 
+onready var radical = preload("res://scenes/UI/GreenTargetRadical.tscn")
+
 var attack = null
 
 var attack_delay = 2
@@ -27,6 +29,10 @@ var timer = null
 
 var can_shoot = true
 
+var state_machine
+
+export var enemy_health : int = 10
+#TO DO when the player shoots at the nef head it dies and goes away
 #timer that has it so that it only shoots one bullet at a time after the player in in range
 func _ready():
 	timer = Timer.new()
@@ -36,13 +42,14 @@ func _ready():
 	add_child(timer)
 	timer.start()
 	
-	
+func _process(_delta):
+	# Enemy expiration after the health is 0.
+	if (enemy_health <= 0) : expire_enemy()
 
 func _physics_process(delta):
 	motion.y = gravity
 	
 	motion.x = speed * direction
-	
 	move_and_slide(motion * delta)
 	# changes direction 
 	for i in get_slide_count():
@@ -59,7 +66,7 @@ func _physics_process(delta):
 			value = 0
 			ray.set_rotation_degrees(Vector3(0,0,-89.21))
 			$laser_muzzle.set_rotation_degrees(Vector3(0,0,0))
-
+	
 
 func nef_head_shoot_time():
 	can_shoot = true
@@ -74,3 +81,34 @@ func _on_player_finding_player_seen():
 		attack.global_rotation = $laser_muzzle.global_rotation
 		can_shoot = false
 		timer.start()
+
+# Adds an active radical.
+func add_active_radical():
+	var g_t_r = radical.instance()
+	if (!self.has_node("res://scenes/UI/GreenTargetRadical.tscn")):
+		self.add_child(g_t_r)
+		
+# Removes an active radical.
+func remove_active_radical():
+	var d_l = self.get_children()
+	for c in d_l:
+		if (c.name == "GreenTargetRadical"):
+			c.queue_free()
+# Act when the mouse has entered the base.
+func _on_nef_head_mouse_entered():
+	add_active_radical()
+
+
+func _on_nef_head_mouse_exited():
+	remove_active_radical()
+# Called when enemy's health is 0.
+func expire_enemy():
+	queue_free()
+
+# Called when damage is dealt to the enemy.
+func damage_enemy(health : int):
+	enemy_health -= health
+
+func _on_AreaEnemy_area_entered(area):
+	if (area.name == "ProjectileExplosionArea"):
+		damage_enemy(2)
