2818579c831addce5819cabab35cc958703b1121
Joonas Heinonen <joonas.heinonen@edu.turkuamk.fi>
2023-07-30 23:59:04 +0300

    joonas-expand-test-level: Mia created.

---
+++ src/scripts/npc/friendly/character.gd
@@ -0,0 +1,26 @@
+extends KinematicBody
+
+export(String, "Right", "Left") var direction
+
+var player
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	player = get_parent().get_parent().get_parent().get_node('player')
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	match (direction):
+		"Right":
+			$CharacterSprite.flip_h = false
+		"Left":
+			$CharacterSprite.flip_h = true
+
+func _physics_process(delta):
+	turn_character(player.translation.x, self.translation.x)
+
+func turn_character(player_x : float, character_x : float):
+	if (player_x > character_x):
+		direction = "Right"
+	elif (player_x < character_x):
+		direction = "Left"
