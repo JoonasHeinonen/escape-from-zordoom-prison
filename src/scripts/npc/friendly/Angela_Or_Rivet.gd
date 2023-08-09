1d8911c3a726f48b7692be102783f6da97a78c9e
Nathan Center <34755641+NathanCenter@users.noreply.github.com>
2023-08-08 20:20:24 -0700

    Added Angela and Rivet sprites

--- src/scripts/npc/friendly/Angela_Or_Rivet.gd
+++ src/scripts/npc/friendly/Angela_Or_Rivet.gd
@@ -1,11 +1,12 @@
 extends KinematicBody
 
-# to do : add angela sprite
-# to do : add rivet sprite
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	#loads texture through code
-	get_node("Sprite3D").set_texture(load("res://icon.png"))
+	
 	pass # Replace with function body.
 
+func _input(event):
+	if (Globle.player_character == "Angela"):
+		$CharacterSprite_Angela.hide()
+	if (Globle.player_character == "Rivet"):
+		$CharacterSprite_Rivet.hide()
