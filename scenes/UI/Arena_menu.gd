a099216bfd9b30cde8fa8949ba0ee385ab4590a1
NathanCenter <nathanjcenter@gmail.com>
2023-12-09 14:03:57 -0700

    cleaner more readable code at least to me wbu?

--- scenes/UI/Arena_menu.gd
+++ scenes/UI/Arena_menu.gd
@@ -1,14 +1,14 @@
 extends Control
 
-var badGuy
+var bad_guy_instance
 var nodes
 
-var is_open = false
-var open : bool
-var pause : bool
+var check_is_open = false
+var has_open : bool
+var has_pause : bool
 
 onready var arena_button = $buttons/ArenaButton
-onready var player     = get_parent().get_parent()
+onready var player = get_parent().get_parent()
 onready var exit_button = $buttons/ExitButton
 
 const bad_guy_nef_head = preload("res://scenes/NPC/Enemies/nef_head_enemy.tscn")
@@ -20,24 +20,24 @@ func _ready():
 func _input(event):
 	if (!Globle.arena_menu_open):
 		if (Globle.arena_menu_active == true):
-			Arena_Menu_process(true, false)
+			open_and_closes_Arena_Menu(true, false)
 			show()
 
-func Arena_Menu_process(open, pause):
+func open_and_closes_Arena_Menu(has_open, has_pause):
 	Globle.update_vendor()
-	get_tree().paused = open
-	get_parent().set_process_input(pause)
+	get_tree().paused = has_open
+	get_parent().set_process_input(has_pause)
 
 # Opens the menu.
 func opens_Menu():
-	if not is_open:
-		is_open = true
+	if not check_is_open:
+		check_is_open = true
 		arena_button.grab_focus()
 		show()
 
 #Exits out of the menu.
 func close_Menu():
-	is_open = false
+	check_is_open  = false
 	Globle.arena_menu_open = false
 	hide()
 
@@ -57,8 +57,8 @@ func _on_ArenaButton_pressed():
 #and add on where we can do as many fights/challenges as we can code in this one script!
 func spawn_bad_guys_in_fight_1():
 	nodes = get_tree().get_nodes_in_group("badGuySpawn1")
-	badGuy = bad_guy_nef_head.instance()
-	nodes[0].add_child(badGuy)
+	bad_guy_instance = bad_guy_nef_head.instance()
+	nodes[0].add_child(bad_guy_instance)
 	
 # This checks and see if the bad guy/guys in the arena is killed by the player.
 func check_nef_head_is_dead():
@@ -68,6 +68,7 @@ func check_nef_head_is_dead():
 		if node.is_dead:
 			player_wins_fight_1()
 			return
+
 #Player returns to shark man and the fight starts over again.
 func player_wins_fight_1():
 	nodes = get_tree().get_nodes_in_group("playerReturnPostion")
