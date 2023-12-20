d4e35ac85171f86dc9b9cf1d389a0b2a473dc0bf
NathanCenter <nathanjcenter@gmail.com>
2023-12-19 20:11:17 -0700

    got images and text to work in character selection

--- src/scripts/UI/character_selection_button.gd
+++ src/scripts/UI/character_selection_button.gd
@@ -1,14 +1,19 @@
 extends "res://src/scripts/Menu/MenuButton.gd"
 
-@export_enum("Angela", "Rivet") var characters : String
+@export var characters : String
 var res_string: String = ""
 
 func _ready():
+	# it seems to be missing the chara selection files
 	res_string = "res://resources/images/characters/player/%s/%s_character_selection.png"
+	#res_string = "res://resources/images/characters/player/"
+	#$CharacterImage.texture = load(res_string % [characters.to_lower(), characters.to_lower()])
+	print(characters)
 	$CharacterImage.texture = load(res_string % [characters.to_lower(), characters.to_lower()])
 	$CharacterPanel/CharacterName.text = characters
 
 func _on_CharacterSelectionButton_pressed():
+	print(characters)
 	Globle.player_character = characters
 	get_tree().change_scene_to_file(scene_to_load)
 
