719ef86a0c968585a205e0fb6776a9c2e2722059
NathanCenter <nathanjcenter@gmail.com>
2024-02-05 19:16:16 -0700

    fixed bug where nef head laser was not hitting the player

--- src/scripts/npc/enemies/nef_laser.gd
+++ src/scripts/npc/enemies/nef_laser.gd
@@ -9,9 +9,10 @@ func _physics_process(delta):
 	var direction = -1 if is_left else 1
 	velocity.x = bullet_speed * delta * 1 * direction
 	translate(velocity)
-	
-func _on_KinematicBody_body_entered(body):
+
+func _on_area_3d_body_entered(body):
 	if (body.name == "player"):
 		body.damage_player(1)
+		print("enter body")
 	if !body.is_in_group("nef_head") :
 		queue_free()
