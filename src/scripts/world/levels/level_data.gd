17a00e19293274b7544fe54856d7f1065bff2548
Joonas Kristian Heinonen <joonas.heinonen@wunder.io>
2024-02-26 19:02:19 +0200

    godot4: Level data script enhanced.

--- src/scripts/world/levels/level_data.gd
+++ src/scripts/world/levels/level_data.gd
@@ -2,13 +2,13 @@ extends Node
 
 class_name LevelData
 
-var z_axis = 0
+@export var z_axis : float
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	if (get_node("npc/friendly")):
 		for npc in $npc/friendly.get_children():
-			npc.position.z = -1.25
+			npc.position.z = z_axis
 			print(npc.position.z)
 	else:
 		printerr("Does not exist...")
