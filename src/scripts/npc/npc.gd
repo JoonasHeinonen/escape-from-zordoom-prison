e1d88da3e08c10f77bd71a1e057fa3da1d4350a9
NathanCenter <34755641+NathanCenter@users.noreply.github.com>
2024-03-01 21:38:58 -0700

    fixed a minor bug talking head was not repeating

--- src/scripts/npc/npc.gd
+++ src/scripts/npc/npc.gd
@@ -53,8 +53,9 @@ func _input(event):
 								commence_dialog('test_timeline')
 								shark_man_dialog_value = process_dialog_value(npc_angela_rivet_dialog_value, 1)
 							(1):
-								print(shark_man_dialog_value)
+								shark_man_dialog_value = process_dialog_value(npc_angela_rivet_dialog_value, 0)
 								Globle.arena_menu_open = true
+								#cant seem to disable player movement yet.
 	if (Globle.player_character == "Rivet" and get_node_or_null('DialogNode') == null and Input.is_action_just_pressed("ui_accept") and active == true):
 		match(character_name):
 			"Mia":
