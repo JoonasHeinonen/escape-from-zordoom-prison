52b55bb222c2eb2afcce0466579df22518c88314
Joonas Kristian Heinonen <joonas.heinonen@wunder.io>
2024-03-02 20:49:04 +0200

    godot4: Blitz gun projectile enhanced.

--- src/scripts/effects/bullets/blaster_projectile.gd
+++ src/scripts/effects/bullets/blaster_projectile.gd
@@ -1,10 +1,11 @@
 extends ProjectileBase
 
 @onready var blaster_projectile_explosion = preload("res://scenes/Effects/ProjectileEffects/BlasterProjectileExplosion.tscn")
+@onready var blitz_gun_projectile_explosion = preload("res://scenes/Effects/ProjectileEffects/BlitzGunProjectileExplosion.tscn")
 @onready var miniturret_projectile_explosion = preload("res://scenes/Effects/ProjectileEffects/MiniturretProjectileExplosion.tscn")
 @onready var projectile_effect = preload("res://scenes/Effects/ProjectileEffects/BlasterProjectileEffect.tscn")
 
-@export_enum("blaster", "miniturret") var weapon: String
+@export_enum("blaster", "miniturret", "blitz_gun") var weapon: String
 
 func _ready():
 	velocity = Vector3(0, 0, 0)
@@ -23,16 +24,19 @@ func _on_Area_body_entered(_body):
 	match (weapon):
 		"blaster":
 			var explosion = blaster_projectile_explosion.instantiate()
-			get_tree().current_scene.add_child(explosion)
-			explosion.global_transform = $Explosion.global_transform
-			explosion.rotation = Vector3.ZERO
-			queue_free()
+			generate_explosion(explosion)
 		"miniturret":
 			var explosion = miniturret_projectile_explosion.instantiate()
-			get_tree().current_scene.add_child(explosion)
-			explosion.global_transform = $Explosion.global_transform
-			explosion.rotation = Vector3.ZERO
-			queue_free()
+			generate_explosion(explosion)
+		"blitz_gun":
+			var explosion = blitz_gun_projectile_explosion.instantiate()
+			generate_explosion(explosion)
+
+func generate_explosion(explosion):
+	get_tree().current_scene.add_child(explosion)
+	explosion.global_transform = $Explosion.global_transform
+	explosion.rotation = Vector3.ZERO
+	queue_free()
 
 func _on_CombustionTimer_timeout():
 	emit_blaster_combustion_effect()
--- src/scripts/effects/bullets/blitz_gun_projectile.gd
+++ src/scripts/effects/bullets/blitz_gun_projectile.gd
@@ -15,9 +15,8 @@ func _physics_process(delta):
 	velocity.x = speed * delta * 1
 	translate(velocity)
 
-func _on_BiltzArea_body_entered(_body):
+func _on_BlitzArea_body_entered(body):
 	var explosion = projectile_explosion.instantiate()
 	get_tree().current_scene.add_child(explosion)
 	explosion.global_transform = $Explosion.global_transform
 	queue_free()
-	pass 
