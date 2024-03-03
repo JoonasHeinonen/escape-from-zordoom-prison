4d3348e4c749b84a51b30f4440ef9f2784b9f4d1
Joonas Kristian Heinonen <joonas.heinonen@wunder.io>
2024-03-03 21:32:00 +0200

    godot4: Negotiator projectile projectile fixed.

--- src/scripts/effects/bullets/blaster_projectile_explosion.gd
+++ src/scripts/effects/bullets/blaster_projectile_explosion.gd
@@ -4,5 +4,10 @@ func _ready():
 	$KillTimer.connect("timeout", Callable(self, "_on_KillTimer_timeout"))
 	$KillTimer.start()
 
+func _physics_process(_delta):
+	self.rotation.x = 0
+	self.rotation.y = 0
+	self.rotation.z = 0
+
 func _on_KillTimer_timeout():
 	queue_free()
