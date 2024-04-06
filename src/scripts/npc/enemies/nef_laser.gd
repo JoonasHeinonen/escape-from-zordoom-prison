a0f7968cffb518e0a34374a396ed4c054ee89ed5
NathanCenter <34755641+NathanCenter@users.noreply.github.com>
2024-04-05 21:32:37 -0700

    fixed up shark npc and added test script for nef head

--- src/scripts/npc/enemies/nef_laser.gd
+++ src/scripts/npc/enemies/nef_laser.gd
@@ -15,6 +15,6 @@ func _physics_process(delta):
 func _on_area_3d_body_entered(body):
 	if (body.name == "player"):
 		body.damage_player(1)
-		print("enter body")
+		#print("enter body")
 	if !body.is_in_group("nef_head") :
 		queue_free()
