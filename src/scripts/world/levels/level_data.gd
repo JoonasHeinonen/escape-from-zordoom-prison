6d5091e32c4197c8c2d3090b643129d57a8b7b7b
Joonas Heinonen <mrdanglars@gmail.com>
2024-05-04 00:34:01 +0300

    music: Initial code for the scene-specific music.

--- src/scripts/world/levels/level_data.gd
+++ src/scripts/world/levels/level_data.gd
@@ -3,9 +3,21 @@ extends Node
 class_name LevelData
 
 @export var z_axis : float
+# AudioStreamPlayer3D
+@export var music_stream : AudioStreamMP3
+
+var music = AudioStreamPlayer.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	add_child(music)
+
+	if (music_stream):
+		music.set_stream(music_stream)
+		music.play()
+	else:
+		printerr("No musical track was provided for this level.")
+
 	if (get_node("npc/friendly")):
 		for npc in $npc/friendly.get_children():
 			npc.position.z = z_axis
