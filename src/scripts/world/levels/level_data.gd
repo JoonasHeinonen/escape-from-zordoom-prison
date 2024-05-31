f7b0b224899e088027ef92999f774798a4897f13
Joonas Heinonen <mrdanglars@gmail.com>
2024-05-31 14:07:24 +0300

    more-levels: Mission parameters added.

--- src/scripts/world/levels/level_data.gd
+++ src/scripts/world/levels/level_data.gd
@@ -4,6 +4,8 @@ class_name LevelData
 
 @export var z_axis : float
 @export var default_music_stream : AudioStreamMP3
+## TODO Specify proper data types for key and value: <String><bool>
+@export var mission_params : Dictionary = {}
 
 var default_music = AudioStreamPlayer.new()
 
@@ -12,6 +14,9 @@ var player = null
 func _ready():
 	default_music_stream.loop = true
 	default_music.set_stream(default_music_stream)
+
+	## TODO Use the following pattern to add new mission parameters.
+	mission_params.merge({ "newMission": false })
 
 	add_child(default_music)
 	player = get_node("player")
