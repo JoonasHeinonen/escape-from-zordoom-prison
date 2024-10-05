79ca6bc1b59e86723d952bf2e7f46161f04a7e94
Joonas Kristian Heinonen <joonas.heinonen@wunder.io>
2024-10-05 21:07:45 +0300

    F44 added.

--- src/scripts/npc/npc.gd
+++ src/scripts/npc/npc.gd
@@ -2,7 +2,7 @@ extends Area3D
 
 class_name NPC
 
-@export_enum("Mia", "Girdeux" , "NPC_Angela_Rivet", "Shark_man") var character_name: String
+@export_enum("Mia", "Girdeux" , "NPC_Angela_Rivet", "Shark_man", "F44") var character_name: String
 
 var active : bool = false
 
@@ -59,6 +59,8 @@ func _input(_event):
 								shark_man_dialog_value = process_dialog_value(npc_angela_rivet_dialog_value, 0)
 								Globle.player_active = true
 								Globle.arena_menu_open = true
+					"F44":
+						print("Ce soir la vie n'est plus un enfer pour 1 fois.")
 	if (Globle.player_character == "Rivet" and get_node_or_null('DialogNode') == null and Input.is_action_just_pressed("ui_accept") and active == true):
 		match(character_name):
 			"Mia":
@@ -87,6 +89,8 @@ func _input(_event):
 						shark_man_dialog_value = process_dialog_value(npc_angela_rivet_dialog_value, 0)
 						Globle.player_active = true
 						Globle.arena_menu_open = true
+			"F44":
+				print("Ce soir la vie n'est plus un enfer pour 2 fois.")
 
 	if (self.has_node("EnterButton")):
 		$EnterButton.visible = active
