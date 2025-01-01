6d7fd9bbf4a31647bd9754e959fc479661a4259d
Joonas Heinonen <mrdanglars@gmail.com>
2025-01-01 23:18:39 +0200

    Optimized code.

--- src/scripts/effects/water_shader_effect.gd
+++ src/scripts/effects/water_shader_effect.gd
@@ -1,13 +1,6 @@
 extends Node3D
 
-
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	var sim_viewport : SubViewport = $Simulation
 	var sim_material: ShaderMaterial = $Simulation/ColorRect.material
 	sim_material.set_shader_parameter('sim_text',sim_viewport.get_texture())
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
